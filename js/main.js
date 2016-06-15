28065841130f1a6e34184d49e5ae4240e3b0dde5
Taylor Milliman <twm013@bucknell.edu>
2016-06-15 15:14:28 -0400

    Everything appears to be working

--- js/main.js
+++ js/main.js
@@ -39,53 +39,56 @@ function GameManager(){
   this.playerTurn = false;
   this.strict = false;
   this.pattern = [];
-  this.game;
 }
 
 GameManager.prototype.start = function(){
   this.startLevel();
-  enableAll();
 }
 
 GameManager.prototype.getPlayerTurn = function(){
   return this.playerTurn;
 }
 
-
 GameManager.prototype.startLevel = function(){
+  disableAll();
   this.playerTurn = false;
   this.level+=1;
   this.playerIndex = 0;
   addOne(this.pattern);
   this.playPattern();
-  $("#count").text(this.level);
+  if (this.level > 20){
+    onWin(this);
+  }
 }
 
 GameManager.prototype.reset = function(){
-  this.level=1;
+  this.level=0;
   this.playerIndex = 0;
   this.playerTurn = false;
-  this.strict = false;
   this.pattern = [];
-  this.game = null;
+  var game = this;
+  setTimeout(function(game){
+    game.updateFromModel();
+  }, 500, game);
 }
 
 GameManager.prototype.checkInput = function(btn_index){
   var actual = this.pattern[this.playerIndex];
-  console.log("actual: " + actual)
-  console.log("player: " + btn_index);
   if (actual != btn_index){
     this.playerTurn = false;
     if (this.strict){
-      this.reset();
+      onLoss(this);
     }
     else{
       this.playPattern();
+      this.playerIndex = 0;
+      this.updateFromModel();
     }
     $("#count").text("!!!");
   }
   else{
     this.playerIndex+=1;
+    $("#count").text($("#count").text() - 1);
     if (this.playerIndex == this.level){
       this.startLevel();
     }
@@ -94,6 +97,11 @@ GameManager.prototype.checkInput = function(btn_index){
 
 GameManager.prototype.updateFromModel = function(){
   $("#count").text(this.level);
+  $("#score").text("Score: " + this.level);
+  var high = $("#high").text();
+  if (this.level > high){
+    $("#high").text(this.level);
+  }
 }
 
 GameManager.prototype.setStrict = function(val){
@@ -101,12 +109,25 @@ GameManager.prototype.setStrict = function(val){
 }
 
 GameManager.prototype.playPattern = function(){
+  var delay = 1000;
+  var game = this;
   for (var i=0; i < this.pattern.length; i++){
     var btn_index = this.pattern[i];
     var id = buttons[btn_index];
-    clickButton(id);
+  (function(currId){
+    setTimeout(function(){
+      clickButton(currId);
+    }, delay);
+  })(id);
+    delay += 1000;
+    if (i==this.pattern.length-1){
+      setTimeout(function(){
+        game.playerTurn = true;
+        game.updateFromModel();
+        enableAll();
+      }, delay);
+    }
   }
-  this.playerTurn = true;
 }
 
 
@@ -133,4 +154,22 @@ function enableAll(){
 function disableAll(){
   $(".game").children("button").attr("disabled", "disabled");
   $(".start").attr("disabled", "disabled");
+}
+
+function onWin(game){
+  $(".modal-title").text("You Won!  Would you like to play again?");
+  $("#replayModal").modal("show");
+  $("#Yes").click(function(){
+    game.reset();
+    game.start();
+  });
+}
+
+function onLoss(game){
+  $(".modal-title").text("You Lost!  Would you like to play again?");
+  $("#replayModal").modal("show");
+  $("#Yes").click(function(){
+    game.reset();
+    game.start();
+  });
 }
